cd70d2d1f39c777636c21c6d7c300b1ecf452860
SARMADSPC\sarma <sarmad9@hotmail.de>
2022-11-07 10:30:18 +0100

    make it better

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,11 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
